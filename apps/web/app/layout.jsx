153c24dcf7cc524c64a18ffb34c79ed965f7cc9a
Shreyan Kundu <kundu.shreyan7@gmail.com>
2026-09-18 04:26:00 +0530

    responsive

--- apps/web/app/layout.jsx
+++ apps/web/app/layout.jsx
@@ -8,6 +8,10 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, viewport-fit=cover"
+                />
                 <link
                     rel="stylesheet"
                     href="/assets/styles.css"
